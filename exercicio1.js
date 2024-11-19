ddeba541752989634ba04b01b5271841fc4ca95b
Lucas Andre cardoso de Lima <lucas2004@gmail.com>
2024-11-19 20:20:42 -0300

    first commit

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+let numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+console.log(numbers);
+
+let removed = numbers.pop();
+console.log(removed);
+console.log(numbers);
+
+let index = numbers.indexOf(30);
+console.log(index);
